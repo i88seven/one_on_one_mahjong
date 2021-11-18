06a379b3e5859da7d8173660d0f81236b0d0aa9c
i88seven <i88.jelounjem@gmail.com>
2021-11-19 00:38:14 +0900

    Enable to fetch separated tiles candidates

--- lib/utils/mahjong_util.dart
+++ lib/utils/mahjong_util.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+
 import 'package:one_on_one_mahjong/constants/all_tiles.dart';
 
 List<AllTileKinds> convertRedTiles(List<AllTileKinds> tiles) {
@@ -13,4 +16,163 @@ List<AllTileKinds> convertRedTiles(List<AllTileKinds> tiles) {
     }
     return tile;
   }).toList();
+}
+
+AllTileKinds? addTileNumber(AllTileKinds? tile) {
+  if (tile == null) {
+    return null;
+  }
+  Map<AllTileKinds, AllTileKinds> specialTileMap = {
+    AllTileKinds.m5: AllTileKinds.m6,
+    AllTileKinds.p5: AllTileKinds.p6,
+    AllTileKinds.s5: AllTileKinds.s6,
+  };
+  if (specialTileMap.keys.contains(tile)) {
+    return specialTileMap[tile];
+  }
+  if (tile.name.startsWith('j')) {
+    return null;
+  }
+  if (tile.name.endsWith('9')) {
+    return null;
+  }
+  int tileIndex = AllTileKinds.values.indexOf(tile);
+  return AllTileKinds.values[tileIndex + 1];
+}
+
+bool canExtractChow(List<AllTileKinds> tiles, AllTileKinds baseTile) {
+  if (baseTile.name.startsWith('j')) {
+    return false;
+  }
+  if (baseTile.name.endsWith('8') || baseTile.name.endsWith('9')) {
+    return false;
+  }
+  Map<AllTileKinds, int> tileKindCount = fetchTileKindCount(tiles);
+  AllTileKinds? nextTile;
+  for (var i = 0; i < 2; i++) {
+    nextTile = addTileNumber(nextTile ?? baseTile);
+    if (nextTile == null || !tileKindCount.keys.contains(nextTile)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void sortSeparatedTiles(List<Map<String, AllTileKinds>> separatedTiles) {
+  separatedTiles.sort((a, b) {
+    if (a.keys.toList()[0] == 'head') {
+      return -1;
+    }
+    if (b.keys.toList()[0] == 'head') {
+      return 1;
+    }
+    if (a.keys.toList()[0] == 'pung' && b.keys.toList()[0] == 'chow') {
+      return -1;
+    }
+    if (a.keys.toList()[0] == 'chow' && b.keys.toList()[0] == 'pung') {
+      return 1;
+    }
+    return AllTileKinds.values.indexOf(a.values.toList()[0]) -
+        AllTileKinds.values.indexOf(b.values.toList()[0]);
+  });
+}
+
+bool isSeparatedTilesFound(
+  List<List<Map<String, AllTileKinds>>> separatedTilesCandidates,
+  List<Map<String, AllTileKinds>> targetSeparatedTiles,
+) {
+  if (separatedTilesCandidates.isEmpty || targetSeparatedTiles.isEmpty) {
+    return false;
+  }
+  for (List<Map<String, AllTileKinds>> separatedTilesCandidate
+      in separatedTilesCandidates) {
+    bool result = true;
+    for (var i = 0; i < targetSeparatedTiles.length; i++) {
+      if (!mapEquals(separatedTilesCandidate[i], targetSeparatedTiles[i])) {
+        result = false;
+        break;
+      }
+    }
+    if (result) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void fetchSetRecursively(
+  List<AllTileKinds> tiles,
+  List<List<Map<String, AllTileKinds>>> separatedTilesCandidates,
+  List<Map<String, AllTileKinds>> currentSeparatedMaps,
+) {
+  if (tiles.length <= 2) {
+    if (tiles[0] == tiles[1]) {
+      currentSeparatedMaps.add({'head': tiles[0]});
+      sortSeparatedTiles(currentSeparatedMaps);
+      if (!isSeparatedTilesFound(
+          separatedTilesCandidates, currentSeparatedMaps)) {
+        separatedTilesCandidates.add(currentSeparatedMaps);
+      }
+    }
+    return;
+  }
+  List<AllTileKinds> pungCandidate = fetchPungCandidate(tiles);
+  List<AllTileKinds> chowCandidate = fetchChowCandidate(tiles);
+  for (AllTileKinds pung in pungCandidate) {
+    List<AllTileKinds> pungRemainderTiles = [...tiles];
+    for (var i = 0; i < 3; i++) {
+      pungRemainderTiles.remove(pung);
+    }
+    fetchSetRecursively(pungRemainderTiles, separatedTilesCandidates, [
+      ...currentSeparatedMaps,
+      {'pung': pung}
+    ]);
+  }
+  for (AllTileKinds chow in chowCandidate) {
+    List<AllTileKinds> chowRemainderTiles = [...tiles];
+    chowRemainderTiles.remove(chow);
+    chowRemainderTiles.remove(addTileNumber(chow));
+    chowRemainderTiles.remove(addTileNumber(addTileNumber(chow)));
+    fetchSetRecursively(chowRemainderTiles, separatedTilesCandidates, [
+      ...currentSeparatedMaps,
+      {'chow': chow}
+    ]);
+  }
+}
+
+List<List<Map<String, AllTileKinds>>> fetchSeparatedTilesCandidates(
+    List<AllTileKinds> tiles) {
+  List<List<Map<String, AllTileKinds>>> separatedTilesCandidates = [];
+  fetchSetRecursively(convertRedTiles(tiles), separatedTilesCandidates, []);
+  return separatedTilesCandidates;
+}
+
+List<AllTileKinds> fetchPungCandidate(List<AllTileKinds> tiles) {
+  Map<AllTileKinds, int> tileKindCount = fetchTileKindCount(tiles);
+  tileKindCount.removeWhere((key, value) => value < 3);
+  return tileKindCount.keys.toList();
+}
+
+List<AllTileKinds> fetchChowCandidate(List<AllTileKinds> tiles) {
+  Map<AllTileKinds, int> tileKindCount = fetchTileKindCount(tiles);
+  List<AllTileKinds> result = [];
+  for (AllTileKinds tileKind in tileKindCount.keys) {
+    if (canExtractChow(tileKindCount.keys.toList(), tileKind)) {
+      result.add(tileKind);
+    }
+  }
+  return result;
+}
+
+Map<AllTileKinds, int> fetchTileKindCount(List<AllTileKinds> tiles) {
+  List<AllTileKinds> convertedTiles = convertRedTiles(tiles);
+  Map<AllTileKinds, int> tileKindCount = {};
+  for (AllTileKinds tile in convertedTiles) {
+    if (tileKindCount[tile] == null) {
+      tileKindCount[tile] = 1;
+      continue;
+    }
+    tileKindCount[tile] = tileKindCount[tile]! + 1;
+  }
+  return tileKindCount;
 }
--- test/mahjong_util_test.dart
+++ test/mahjong_util_test.dart
@@ -5,7 +5,7 @@ import 'package:one_on_one_mahjong/utils/mahjong_util.dart';
 void main() {
   group('convertRedTiles test', () {
     test('convertRedTiles', () {
-      List<AllTileKinds> _hands = [
+      List<AllTileKinds> hands = [
         AllTileKinds.m5,
         AllTileKinds.m5,
         AllTileKinds.mr,
@@ -20,7 +20,7 @@ void main() {
         AllTileKinds.j7,
         AllTileKinds.j7,
       ];
-      final convertedTiles = convertRedTiles(_hands);
+      final convertedTiles = convertRedTiles(hands);
       List<AllTileKinds> expectTiles = [
         AllTileKinds.m5,
         AllTileKinds.m5,
@@ -38,8 +38,250 @@ void main() {
       ];
       expect(convertedTiles.length, expectTiles.length);
       for (AllTileKinds convertedTile in convertedTiles) {
-        expect(expectTiles.contains(convertedTile), true);
+        expect(expectTiles.contains(convertedTile), true,
+            reason: "for tile:${convertedTile.name}");
       }
+    });
+  });
+
+  test('addTileNumber', () {
+    Map<AllTileKinds, AllTileKinds?> expectTilesMap = {
+      AllTileKinds.m1: AllTileKinds.m2,
+      AllTileKinds.m4: AllTileKinds.m5,
+      AllTileKinds.m5: AllTileKinds.m6,
+      AllTileKinds.mr: AllTileKinds.m6,
+      AllTileKinds.m6: AllTileKinds.m7,
+      AllTileKinds.m9: null,
+      AllTileKinds.p1: AllTileKinds.p2,
+      AllTileKinds.p4: AllTileKinds.p5,
+      AllTileKinds.p5: AllTileKinds.p6,
+      AllTileKinds.pr: AllTileKinds.p6,
+      AllTileKinds.p6: AllTileKinds.p7,
+      AllTileKinds.p9: null,
+      AllTileKinds.s1: AllTileKinds.s2,
+      AllTileKinds.s4: AllTileKinds.s5,
+      AllTileKinds.s5: AllTileKinds.s6,
+      AllTileKinds.sr: AllTileKinds.s6,
+      AllTileKinds.s6: AllTileKinds.s7,
+      AllTileKinds.s9: null,
+      AllTileKinds.j1: null,
+      AllTileKinds.j4: null,
+      AllTileKinds.j5: null,
+      AllTileKinds.j7: null,
+    };
+    for (AllTileKinds baseTile in expectTilesMap.keys) {
+      expect(addTileNumber(baseTile), expectTilesMap[baseTile],
+          reason: "for tile:${baseTile.name}");
+    }
+  });
+
+  group('canExtractChow test', () {
+    test('canExtractChow red', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.m4,
+        AllTileKinds.mr,
+        AllTileKinds.p1,
+        AllTileKinds.p3,
+        AllTileKinds.p4,
+        AllTileKinds.pr,
+        AllTileKinds.p6,
+        AllTileKinds.p7,
+        AllTileKinds.p9,
+        AllTileKinds.s1,
+        AllTileKinds.s2,
+        AllTileKinds.s3,
+        AllTileKinds.s4,
+        AllTileKinds.s5,
+      ];
+      Map<AllTileKinds, bool> expectTiles = {
+        AllTileKinds.m4: false,
+        AllTileKinds.mr: false,
+        AllTileKinds.p1: false,
+        AllTileKinds.p3: true,
+        AllTileKinds.p4: true,
+        AllTileKinds.pr: true,
+        AllTileKinds.p6: false,
+        AllTileKinds.p7: false,
+        AllTileKinds.p9: false,
+        AllTileKinds.s1: true,
+        AllTileKinds.s2: true,
+        AllTileKinds.s3: true,
+        AllTileKinds.s4: false,
+        AllTileKinds.s5: false,
+      };
+      for (AllTileKinds expectTile in expectTiles.keys) {
+        expect(canExtractChow(hands, expectTile), expectTiles[expectTile],
+            reason: "for tile:${expectTile.name}");
+      }
+    });
+
+    test('canExtractChow twoDoubleRun', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.m3,
+        AllTileKinds.m3,
+        AllTileKinds.m4,
+        AllTileKinds.m4,
+        AllTileKinds.m5,
+        AllTileKinds.mr,
+        AllTileKinds.p1,
+        AllTileKinds.p1,
+        AllTileKinds.p2,
+        AllTileKinds.p2,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p4,
+        AllTileKinds.p4,
+      ];
+      Map<AllTileKinds, bool> expectTiles = {
+        AllTileKinds.m3: true,
+        AllTileKinds.m4: false,
+        AllTileKinds.m5: false,
+        AllTileKinds.mr: false,
+        AllTileKinds.p1: true,
+        AllTileKinds.p2: true,
+        AllTileKinds.p3: false,
+        AllTileKinds.p4: false,
+      };
+      for (AllTileKinds expectTile in expectTiles.keys) {
+        expect(canExtractChow(hands, expectTile), expectTiles[expectTile],
+            reason: "for tile:${expectTile.name}");
+      }
+    });
+  });
+
+  group('fetchPungCandidate test', () {
+    test('fetchPungCandidate', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.mr,
+        AllTileKinds.p5,
+        AllTileKinds.pr,
+        AllTileKinds.s5,
+        AllTileKinds.s5,
+        AllTileKinds.sr,
+        AllTileKinds.s7,
+        AllTileKinds.s7,
+        AllTileKinds.s7,
+        AllTileKinds.s7,
+        AllTileKinds.j5,
+        AllTileKinds.j5,
+        AllTileKinds.j6,
+      ];
+      final candidates = fetchPungCandidate(hands);
+      List<AllTileKinds> expectTiles = [
+        AllTileKinds.s5,
+        AllTileKinds.s7,
+      ];
+      expect(candidates.length, expectTiles.length);
+      for (AllTileKinds candidate in candidates) {
+        expect(expectTiles.contains(candidate), true,
+            reason: "for tile:${candidate.name}");
+      }
+    });
+  });
+
+  group('fetchChowCandidate test', () {
+    test('fetchChowCandidate', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.p1,
+        AllTileKinds.p2,
+        AllTileKinds.p2,
+        AllTileKinds.p3,
+        AllTileKinds.p4,
+        AllTileKinds.p5,
+        AllTileKinds.pr,
+        AllTileKinds.p7,
+        AllTileKinds.p8,
+        AllTileKinds.p8,
+        AllTileKinds.p9,
+        AllTileKinds.sr,
+        AllTileKinds.s6,
+        AllTileKinds.s7,
+      ];
+      final candidates = fetchChowCandidate(hands);
+      List<AllTileKinds> expectTiles = [
+        AllTileKinds.p1,
+        AllTileKinds.p2,
+        AllTileKinds.p3,
+        AllTileKinds.p7,
+        AllTileKinds.s5,
+      ];
+      expect(candidates.length, expectTiles.length);
+      for (AllTileKinds candidate in candidates) {
+        expect(expectTiles.contains(candidate), true,
+            reason: "for tile:${candidate.name}");
+      }
+    });
+  });
+
+  group('fetchSeparatedTilesCandidates test', () {
+    fetchSeparatedTilesCandidatesTest(List<AllTileKinds> hands,
+        List<List<Map<String, AllTileKinds>>> expectCandidates) {
+      final actualCandidates = fetchSeparatedTilesCandidates(hands);
+
+      expect(actualCandidates.length, expectCandidates.length);
+      for (List<Map<String, AllTileKinds>> expectCandidate
+          in expectCandidates) {
+        expect(isSeparatedTilesFound(actualCandidates, expectCandidate), true,
+            reason: "expect: $expectCandidate is not found");
+      }
+    }
+
+    test('fetchSeparatedTilesCandidates test1', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.p2,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p4,
+        AllTileKinds.p4,
+        AllTileKinds.p4,
+        AllTileKinds.p5,
+        AllTileKinds.pr,
+        AllTileKinds.p6,
+        AllTileKinds.p6,
+        AllTileKinds.p8,
+        AllTileKinds.p8,
+      ];
+      List<List<Map<String, AllTileKinds>>> expectCandidates = [
+        [
+          {'head': AllTileKinds.p8},
+          {'pung': AllTileKinds.p3},
+          {'chow': AllTileKinds.p2},
+          {'chow': AllTileKinds.p4},
+          {'chow': AllTileKinds.p4},
+        ]
+      ];
+      fetchSeparatedTilesCandidatesTest(hands, expectCandidates);
+    });
+
+    test('fetchSeparatedTilesCandidates test2', () {
+      List<AllTileKinds> hands = [
+        AllTileKinds.p1,
+        AllTileKinds.p1,
+        AllTileKinds.p1,
+        AllTileKinds.p1,
+        AllTileKinds.p2,
+        AllTileKinds.p2,
+        AllTileKinds.p2,
+        AllTileKinds.p2,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p3,
+        AllTileKinds.p4,
+        AllTileKinds.p4,
+        AllTileKinds.p5,
+      ];
+      List<List<Map<String, AllTileKinds>>> expectCandidates = [
+        [
+          {'head': AllTileKinds.p2},
+          {'pung': AllTileKinds.p1},
+          {'chow': AllTileKinds.p1},
+          {'chow': AllTileKinds.p2},
+          {'chow': AllTileKinds.p3},
+        ]
+      ];
+      fetchSeparatedTilesCandidatesTest(hands, expectCandidates);
     });
   });
 }
