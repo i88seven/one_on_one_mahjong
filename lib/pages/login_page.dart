613f6e17707bcad484028b3d6621304a6506aad5
i88seven <i88.jelounjem@gmail.com>
2022-02-18 00:44:56 +0900

    Move to preparation page on create user

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:one_on_one_mahjong/pages/preparation/main.dart';
-import 'create_room.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -62,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                       );
                       await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return CreateRoomPage(result.user!);
+                          return const PreparationMainPage();
                         }),
                       );
                     } on FirebaseAuthException catch (e) {
