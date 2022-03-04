2fdada9ae961c31f7417cc41e4077e8f59db84ef
i88seven <i88.jelounjem@gmail.com>
2022-03-05 00:52:18 +0900

    Add google_mobile_ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:one_on_one_mahjong/pages/login/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await Firebase.initializeApp();
   runApp(const ProviderScope(child: OneOnOneMahjongApp()));
 }
