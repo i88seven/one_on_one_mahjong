79d080bd0d3e5ad8cc523ff59f1c798d73417dc8
i88seven <i88.jelounjem@gmail.com>
2022-03-21 23:38:45 +0900

    Use WinName on statistics

--- lib/types/statistics/win_name_statistics.dart
+++ lib/types/statistics/win_name_statistics.dart
@@ -2,12 +2,7 @@ import 'package:one_on_one_mahjong/constants/win_name.dart';
 import 'package:one_on_one_mahjong/types/statistics/statistics_item.dart';
 
 class WinNameStatistics implements StatisticsItem {
-  int _mangan;
-  int _haneman;
-  int _baiman;
-  int _sanbaiman;
-  int _yakuman;
-  int _kazoeYakuman;
+  Map<WinName, int> statistics = {};
 
   WinNameStatistics({
     int? mangan,
@@ -16,70 +11,44 @@ class WinNameStatistics implements StatisticsItem {
     int? sanbaiman,
     int? yakuman,
     int? kazoeYakuman,
-  })  : _mangan = mangan ?? 0,
-        _haneman = haneman ?? 0,
-        _baiman = baiman ?? 0,
-        _sanbaiman = sanbaiman ?? 0,
-        _yakuman = yakuman ?? 0,
-        _kazoeYakuman = kazoeYakuman ?? 0;
+  }) {
+    statistics[WinName.mangan] = mangan ?? 0;
+    statistics[WinName.haneman] = haneman ?? 0;
+    statistics[WinName.baiman] = baiman ?? 0;
+    statistics[WinName.sanbaiman] = sanbaiman ?? 0;
+    statistics[WinName.yakuman] = yakuman ?? 0;
+    statistics[WinName.kazoeYakuman] = kazoeYakuman ?? 0;
+  }
 
   @override
-  WinNameStatistics.fromJson(Map<String, int> json)
-      : _mangan = int.parse(json['mangan'].toString()),
-        _haneman = int.parse(json['haneman'].toString()),
-        _baiman = int.parse(json['baiman'].toString()),
-        _sanbaiman = int.parse(json['sanbaiman'].toString()),
-        _yakuman = int.parse(json['yakuman'].toString()),
-        _kazoeYakuman = int.parse(json['kazoeYakuman'].toString());
+  WinNameStatistics.fromJson(Map<String, int> json) {
+    for (WinName winName in WinName.values) {
+      statistics[winName] = int.parse(json[winName.name].toString());
+    }
+  }
 
   @override
   void count(value) {
-    // TODO switch 使わない形にしたい
     WinName winName = value;
-    switch (winName) {
-      case WinName.mangan:
-        _mangan++;
-        break;
-      case WinName.haneman:
-        _haneman++;
-        break;
-      case WinName.baiman:
-        _baiman++;
-        break;
-      case WinName.sanbaiman:
-        _sanbaiman++;
-        break;
-      case WinName.kazoeYakuman:
-        _kazoeYakuman++;
-        break;
-      case WinName.yakuman:
-        _yakuman++;
-        break;
-      default:
-      // NOP
+    if (statistics[winName] != null) {
+      statistics[winName] = statistics[winName]! + 1;
     }
   }
 
   @override
   Map<String, int> toMap() {
-    return {
-      'mangan': _mangan,
-      'haneman': _haneman,
-      'baiman': _baiman,
-      'sanbaiman': _sanbaiman,
-      'yakuman': _yakuman,
-      'kazoeYakuman': _kazoeYakuman,
-    };
+    Map<String, int> result = {};
+    for (WinName winName in WinName.values) {
+      result[winName.name] = statistics[winName] ?? 0;
+    }
+    return result;
   }
 
   Map<String, String> toNameMap() {
-    return {
-      '満貫': _mangan.toString(),
-      '跳満': _haneman.toString(),
-      '倍満': _baiman.toString(),
-      '三倍満': _sanbaiman.toString(),
-      '役満': _yakuman.toString(),
-      '数え役満': _kazoeYakuman.toString(),
-    };
+    Map<String, String> result = {};
+    for (WinName winName in WinName.values) {
+      result[winName.name] = (statistics[winName] ?? 0).toString();
+    }
+    return result;
   }
 }
