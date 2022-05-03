7b9763ef45de2349c88949f74c8beccba576cfc7
i88seven <i88.jelounjem@gmail.com>
2022-05-04 02:36:36 +0900

    Define theme

---
+++ lib/config/theme.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AppColor {
+  static const primaryColorMain = Color(0xFFCBC943);
+  static const primaryBackgroundColor = Color(0xFFB90001);
+  static const errorColor = Color(0xFFDD4B39);
+  static const textBaseColor = Color(0xFF333333);
+  static const textSubColor = Color(0xFF999999);
+}
+
+class AppFontSize {
+  static const xSmall = 10.0;
+  static const small = 12.0;
+  static const midium = 14.0;
+  static const large = 16.0;
+  static const xLarge = 18.0;
+}
+
+final themeProvider = Provider((_ref) {
+  final themeData = ThemeData.light();
+
+  return themeData.copyWith(
+    primaryColor: AppColor.primaryBackgroundColor,
+    hintColor: AppColor.primaryBackgroundColor,
+    errorColor: AppColor.errorColor,
+    dividerColor: AppColor.primaryBackgroundColor,
+    appBarTheme: themeData.appBarTheme.copyWith(
+      backgroundColor: AppColor.primaryBackgroundColor,
+      titleTextStyle: const TextStyle(
+        color: AppColor.primaryColorMain,
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    textTheme: TextTheme(
+      caption: themeData.textTheme.caption?.copyWith(
+        color: AppColor.textBaseColor,
+      ),
+      bodyText1: themeData.textTheme.bodyText1?.copyWith(
+        fontSize: AppFontSize.small,
+        color: AppColor.textBaseColor,
+      ),
+      bodyText2: themeData.textTheme.bodyText2?.copyWith(
+        fontSize: AppFontSize.midium,
+        color: AppColor.textBaseColor,
+      ),
+      overline: themeData.textTheme.overline?.copyWith(
+        color: AppColor.textBaseColor,
+      ),
+      button: themeData.textTheme.button?.copyWith(
+        color: AppColor.primaryColorMain,
+      ),
+      subtitle1: themeData.textTheme.subtitle1?.copyWith(
+        color: AppColor.textBaseColor,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        onPrimary: AppColor.primaryColorMain,
+        primary: AppColor.primaryBackgroundColor,
+        elevation: 0,
+        padding: const EdgeInsets.all(4),
+        textStyle: themeData.textTheme.button?.copyWith(
+          fontWeight: FontWeight.bold,
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        minimumSize: const Size(150, 30),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        primary: AppColor.primaryBackgroundColor,
+        elevation: 0,
+        padding: const EdgeInsets.all(4),
+        textStyle: themeData.textTheme.button?.copyWith(
+          fontWeight: FontWeight.bold,
+        ),
+        side: const BorderSide(
+          color: AppColor.primaryBackgroundColor,
+          width: 1,
+          style: BorderStyle.solid,
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        minimumSize: const Size(150, 30),
+      ),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      labelStyle: TextStyle(
+        color: AppColor.textBaseColor,
+      ),
+      focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+        color: AppColor.primaryBackgroundColor,
+        ),
+      ),
+    ),
+    cardTheme: const CardTheme(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        side: BorderSide(
+          width: 2,
+          color: AppColor.primaryBackgroundColor,
+        ),
+      ),
+    ),
+  );
+});
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:one_on_one_mahjong/config/theme.dart';
 import 'package:one_on_one_mahjong/pages/login/login_page.dart';
 
 void main() async {
@@ -13,14 +14,14 @@ void main() async {
   runApp(const ProviderScope(child: OneOnOneMahjongApp()));
 }
 
-class OneOnOneMahjongApp extends StatelessWidget {
+class OneOnOneMahjongApp extends ConsumerWidget {
   const OneOnOneMahjongApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'タイマン麻雀',
-      theme: ThemeData.dark(),
+      theme: ref.watch(themeProvider),
       home: const LoginPage(),
     );
   }
