de1ecd16c19d1019664649fcf637d7cd08bac5a8
i88seven <i88.jelounjem@gmail.com>
2022-05-15 20:57:28 +0900

    Restrict to overwrite prepare room

--- lib/pages/preparation/room/create.dart
+++ lib/pages/preparation/room/create.dart
@@ -2,25 +2,65 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:one_on_one_mahjong/components/preparation_background.dart';
+import 'package:one_on_one_mahjong/config/theme.dart';
 import 'package:one_on_one_mahjong/pages/preparation/room/room_id_input.dart';
 import 'package:one_on_one_mahjong/pages/preparation/room/wait.dart';
 import 'package:one_on_one_mahjong/provider/game_user_model.dart';
 
-class RoomCreatePage extends ConsumerWidget {
+class RoomCreatePage extends ConsumerStatefulWidget {
   const RoomCreatePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _RoomCreatePageState createState() => _RoomCreatePageState();
+}
+
+class _RoomCreatePageState extends ConsumerState<RoomCreatePage> {
+  late String _myUid;
+  late String _myName;
+  bool _hasResult = false;
+
+  @override
+  void initState() {
+    super.initState();
     final gameUserModel = ref.read(gameUserProvider);
-    final _myUid = gameUserModel.gameUser.uid;
-    final _myName = gameUserModel.gameUser.name;
+    _myUid = gameUserModel.gameUser.uid;
+    _myName = gameUserModel.gameUser.name;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool _canCreateRoom(DocumentSnapshot roomSnapshot) {
+      if (!roomSnapshot.exists) {
+        return true;
+      }
+      final roomData = roomSnapshot.data()! as Map<String, dynamic>;
+      if (roomData['hostUid'] == _myUid) {
+        return true;
+      }
+      final createdAt = roomData['createdAt'];
+      // 作ってから2時間以上経っていたら上書きできる
+      if (createdAt is Timestamp &&
+          DateTime.now().difference(createdAt.toDate()).inHours > 2) {
+        return true;
+      }
+      return false;
+    }
 
     void _createRoom(String roomId) async {
       try {
-        // TODO すでに存在していて、自分以外が作っていたらエラー
         DocumentReference _roomRef = FirebaseFirestore.instance
             .collection('preparationRooms')
             .doc(roomId);
+        DocumentSnapshot snapshot = await _roomRef.get();
+        if (!_canCreateRoom(snapshot)) {
+          setState(() {
+            _hasResult = true;
+          });
+          return;
+        }
+        setState(() {
+          _hasResult = false;
+        });
         _roomRef.set({
           'hostUid': _myUid,
           'hostName': _myName,
@@ -68,6 +108,17 @@ class RoomCreatePage extends ConsumerWidget {
                   buttonText: '作成',
                 ),
               ),
+              if (_hasResult)
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    'すでに使われているIDです。別のIDを指定してください。',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: AppColor.errorColor,
+                    ),
+                  ),
+                ),
             ],
           ),
         ],
