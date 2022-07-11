cd45bc5e37bb52eda9b49d7a51238565ab6ce18e
i88seven <i88.jelounjem@gmail.com>
2022-07-12 01:40:23 +0900

    Refactor detail page variable

--- lib/pages/game_user/detail_page.dart
+++ lib/pages/game_user/detail_page.dart
@@ -9,7 +9,6 @@ import 'package:one_on_one_mahjong/pages/game_user/setting.dart';
 import 'package:one_on_one_mahjong/pages/login/login_page.dart';
 import 'package:one_on_one_mahjong/provider/game_user_model.dart';
 import 'package:one_on_one_mahjong/provider/game_user_statistics_model.dart';
-import 'package:one_on_one_mahjong/types/game_user.dart';
 import 'package:one_on_one_mahjong/types/statistics/game_user_statistics.dart';
 
 class UserDetailPage extends ConsumerStatefulWidget {
@@ -20,7 +19,7 @@ class UserDetailPage extends ConsumerStatefulWidget {
 }
 
 class _UserDetailPageState extends ConsumerState<UserDetailPage> {
-  late GameUser _gameUser;
+  late GameUserModel _gameUserModel;
   late GameUserStatistics _gameUserStatistics;
   final LocalStorage _storage = LocalStorage('one_one_one_mahjong');
 
@@ -28,8 +27,8 @@ class _UserDetailPageState extends ConsumerState<UserDetailPage> {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) {
         return SettingPage(
-            initialName: _gameUser.name,
-            initialIsPlayMusic: _gameUser.isPlayMusic);
+            initialName: _gameUserModel.gameUser.name,
+            initialIsPlayMusic: _gameUserModel.gameUser.isPlayMusic);
       }),
     );
   }
@@ -63,8 +62,7 @@ class _UserDetailPageState extends ConsumerState<UserDetailPage> {
   @override
   void initState() {
     super.initState();
-    final gameUserModel = ref.read(gameUserProvider);
-    _gameUser = gameUserModel.gameUser;
+    _gameUserModel = ref.read(gameUserProvider);
     final gameUserStatisticsModel = ref.read(gameUserStatisticsProvider);
     _gameUserStatistics = gameUserStatisticsModel.gameUserStatistics;
     Future(() async {
@@ -88,7 +86,7 @@ class _UserDetailPageState extends ConsumerState<UserDetailPage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("名前 : ${_gameUser.name}"),
+                  Text("名前 : ${_gameUserModel.gameUser.name}"),
                   Container(
                     alignment: Alignment.center,
                     child: ElevatedButton.icon(
@@ -100,94 +98,95 @@ class _UserDetailPageState extends ConsumerState<UserDetailPage> {
                 ],
               ),
               const SizedBox(height: 16),
-              !_gameUser.isAnonymously
+              !_gameUserModel.gameUser.isAnonymously
                   ? ExpansionTile(
-                expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
-                initiallyExpanded: true,
-                childrenPadding: const EdgeInsets.only(
-                    left: 16.0, right: 16.0, bottom: 16.0),
-                title: const Text(
-                  '対戦集計',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                textColor: AppColor.primaryColorMain,
-                iconColor: AppColor.primaryColorMain,
-                children: convertToStatisticsRow([
-                  "総対戦数 : ${_gameUserStatistics.totalGame} 回",
-                  "通信切断 : ${_gameUserStatistics.disconnectionGame} 回 (${_gameUserStatistics.disconnectionRate} %)",
-                  "勝ち : ${_gameUserStatistics.winGame} 回 (${_gameUserStatistics.winRate} %)",
-                  "負け : ${_gameUserStatistics.loseGame} 回 (${_gameUserStatistics.loseRate} %)",
-                  "引き分け : ${_gameUserStatistics.drawGame} 回 (${_gameUserStatistics.drawRate} %)",
-                ]),
+                      expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+                      initiallyExpanded: true,
+                      childrenPadding: const EdgeInsets.only(
+                          left: 16.0, right: 16.0, bottom: 16.0),
+                      title: const Text(
+                        '対戦集計',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      textColor: AppColor.primaryColorMain,
+                      iconColor: AppColor.primaryColorMain,
+                      children: convertToStatisticsRow([
+                        "総対戦数 : ${_gameUserStatistics.totalGame} 回",
+                        "通信切断 : ${_gameUserStatistics.disconnectionGame} 回 (${_gameUserStatistics.disconnectionRate} %)",
+                        "勝ち : ${_gameUserStatistics.winGame} 回 (${_gameUserStatistics.winRate} %)",
+                        "負け : ${_gameUserStatistics.loseGame} 回 (${_gameUserStatistics.loseRate} %)",
+                        "引き分け : ${_gameUserStatistics.drawGame} 回 (${_gameUserStatistics.drawRate} %)",
+                      ]),
                     )
                   : Container(),
-              !_gameUser.isAnonymously
+              !_gameUserModel.gameUser.isAnonymously
                   ? ExpansionTile(
-                expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
-                initiallyExpanded: true,
-                childrenPadding: const EdgeInsets.only(
-                    left: 16.0, right: 16.0, bottom: 16.0),
-                title: const Text(
-                  '局集計',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                textColor: AppColor.primaryColorMain,
-                iconColor: AppColor.primaryColorMain,
-                children: convertToStatisticsRow([
-                  "総局数 : ${_gameUserStatistics.totalRound} 局",
-                  "流局 : ${_gameUserStatistics.drawnRound} 局",
-                  "勝ち : ${_gameUserStatistics.winRound} 局",
-                  "負け : ${_gameUserStatistics.loseRound} 局",
-                  "満貫 : ${_gameUserStatistics.winName['mangan']} 回",
-                  "跳満 : ${_gameUserStatistics.winName['haneman']} 回",
-                  "倍満 : ${_gameUserStatistics.winName['baiman']} 回",
-                  "三倍満 : ${_gameUserStatistics.winName['sanbaiman']} 回",
-                  "役満 : ${_gameUserStatistics.winName['yakuman']} 回",
-                  "数え役満 : ${_gameUserStatistics.winName['kazoeYakuman']} 回",
-                  "平均勝ち点数 : ${_gameUserStatistics.winPointAverage} 点",
-                  "平均負け点数 : ${_gameUserStatistics.losePointAverage} 点",
-                  "平均勝ち歩数 : ${_gameUserStatistics.winStepAverage} 歩",
-                  "平均負け歩数 : ${_gameUserStatistics.loseStepAverage} 歩",
-                ]),
+                      expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+                      initiallyExpanded: true,
+                      childrenPadding: const EdgeInsets.only(
+                          left: 16.0, right: 16.0, bottom: 16.0),
+                      title: const Text(
+                        '局集計',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      textColor: AppColor.primaryColorMain,
+                      iconColor: AppColor.primaryColorMain,
+                      children: convertToStatisticsRow([
+                        "総局数 : ${_gameUserStatistics.totalRound} 局",
+                        "流局 : ${_gameUserStatistics.drawnRound} 局",
+                        "勝ち : ${_gameUserStatistics.winRound} 局",
+                        "負け : ${_gameUserStatistics.loseRound} 局",
+                        "満貫 : ${_gameUserStatistics.winName['mangan']} 回",
+                        "跳満 : ${_gameUserStatistics.winName['haneman']} 回",
+                        "倍満 : ${_gameUserStatistics.winName['baiman']} 回",
+                        "三倍満 : ${_gameUserStatistics.winName['sanbaiman']} 回",
+                        "役満 : ${_gameUserStatistics.winName['yakuman']} 回",
+                        "数え役満 : ${_gameUserStatistics.winName['kazoeYakuman']} 回",
+                        "平均勝ち点数 : ${_gameUserStatistics.winPointAverage} 点",
+                        "平均負け点数 : ${_gameUserStatistics.losePointAverage} 点",
+                        "平均勝ち歩数 : ${_gameUserStatistics.winStepAverage} 歩",
+                        "平均負け歩数 : ${_gameUserStatistics.loseStepAverage} 歩",
+                      ]),
                     )
                   : Container(),
-              !_gameUser.isAnonymously
+              !_gameUserModel.gameUser.isAnonymously
                   ? ExpansionTile(
-                expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
-                initiallyExpanded: false,
-                childrenPadding: const EdgeInsets.only(
-                    left: 16.0, right: 16.0, bottom: 16.0),
-                title: const Text(
-                  '役集計',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                textColor: AppColor.primaryColorMain,
-                iconColor: AppColor.primaryColorMain,
-                children: convertToStatisticsRow([
-                  "勝ち局数 : ${_gameUserStatistics.winRound} 局",
-                  "",
-                  ...Yaku.values.fold<List<String>>([], (currentList, yaku) {
-                    currentList.add(
-                        "${nameMap[yaku]} : ${_gameUserStatistics.winYaku[yaku.name]} 回");
-                    switch (yaku) {
-                      case Yaku.dora:
-                        currentList.add(
-                            "ドラ合計 : ${_gameUserStatistics.winYaku['doraAll']} 翻");
-                        break;
-                      case Yaku.uraDora:
-                        currentList.add(
-                            "裏ドラ合計 : ${_gameUserStatistics.winYaku['uraDoraAll']} 翻");
-                        break;
-                      case Yaku.redFive:
-                        currentList.add(
-                            "赤ドラ合計 : ${_gameUserStatistics.winYaku['redFiveAll']} 翻");
-                        break;
-                      default:
-                      // NOP
-                    }
-                    return currentList;
-                  }),
-                ]),
+                      expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+                      initiallyExpanded: false,
+                      childrenPadding: const EdgeInsets.only(
+                          left: 16.0, right: 16.0, bottom: 16.0),
+                      title: const Text(
+                        '役集計',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      textColor: AppColor.primaryColorMain,
+                      iconColor: AppColor.primaryColorMain,
+                      children: convertToStatisticsRow([
+                        "勝ち局数 : ${_gameUserStatistics.winRound} 局",
+                        "",
+                        ...Yaku.values.fold<List<String>>([],
+                            (currentList, yaku) {
+                          currentList.add(
+                              "${nameMap[yaku]} : ${_gameUserStatistics.winYaku[yaku.name]} 回");
+                          switch (yaku) {
+                            case Yaku.dora:
+                              currentList.add(
+                                  "ドラ合計 : ${_gameUserStatistics.winYaku['doraAll']} 翻");
+                              break;
+                            case Yaku.uraDora:
+                              currentList.add(
+                                  "裏ドラ合計 : ${_gameUserStatistics.winYaku['uraDoraAll']} 翻");
+                              break;
+                            case Yaku.redFive:
+                              currentList.add(
+                                  "赤ドラ合計 : ${_gameUserStatistics.winYaku['redFiveAll']} 翻");
+                              break;
+                            default:
+                            // NOP
+                          }
+                          return currentList;
+                        }),
+                      ]),
                     )
                   : Container(),
               const SizedBox(height: 52),
